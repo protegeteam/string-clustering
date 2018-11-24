fe75ad8135120da0284516a61e6d255e97eaeecd
Rafael Goncalves <goncalvjr@gmail.com>
2018-11-24 11:06:48 -0800

    Get ontology coverage for a sample of cluster elements
    BioPortal doesn't handle more than ~200 keywords

--- clusterquality.py
+++ clusterquality.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Provides ClusterQuality class"""
 
+import logging
+import random
 import sys
 
 from ontorecommender import OntoRecommender
@@ -14,15 +16,18 @@ class ClusterQuality:
     def __init__(self, out_file, bp_ap_key):
         self.file_writer = open(out_file, 'w')
         self.bp_ap_key = bp_ap_key
+        logging.basicConfig(level=logging.INFO)
 
     def verify(self, cluster_dict):
         recommender = OntoRecommender(self.bp_ap_key)
         for cluster in cluster_dict:
             clust_elements = cluster_dict[cluster]
-            nr_clust_elements = str(len(clust_elements))
-            csv_clust_elements = ",".join(clust_elements)  # string with comma-separated list of cluster elements
+            nr_clust_elements = len(clust_elements)
+            if nr_clust_elements > 150:
+                clust_elements = random.sample(clust_elements, 150)
+            csv_clust_elements = ",".join(clust_elements)  # comma-separated list of up to 150 cluster elements
             ont_acr, ont_id, cov_score, cov_score_norm, cov_terms, cov_words = recommender.recommend(csv_clust_elements)
-            self.file_writer.write(str(cluster) + "," + nr_clust_elements + ",")
+            self.file_writer.write(str(cluster) + "," + str(nr_clust_elements) + ",")
             self.file_writer.write(ont_acr + "," + ont_id + "," + str(cov_score) + "," + str(cov_score_norm) + "," +
                                    str(cov_terms) + "," + str(cov_words) + "\n")
         self.file_writer.close()
